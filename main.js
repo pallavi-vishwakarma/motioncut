ebe9744bcad90d620b5b00e403e8c50d46129d00
pallavi-vishwakarma <152671865+pallavi-vishwakarma@users.noreply.github.com>
2024-01-21 20:53:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// function add_list(){
+//     let x = document.getElementById("box");
+//     let y  = document.getElementById("list");
+
+//     if (x.value !=''){
+//            let make_li = document.createElement("li");
+//            let delete = document.createElement("button");
+//            delete.onclick = function() {
+//             delete.remove
+//           }
+//           let complete = deleteButton + listitem;
+        
+//           return complete;
+
+//            make_li.appendChild(document.createTextNode(x.value));
+//         y.appendChild(make_li);
+//         x.value =""
+        
+//     }
+// }
